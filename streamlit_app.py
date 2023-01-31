3bcacca8e0c5a9b60ced5fb9a6be10de3f694516
A-M-N-23 <122982774+A-M-N-23@users.noreply.github.com>
2023-01-31 13:51:26 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,3 +31,6 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
+
+# Tell py file to use the library added to my project
+import snowflake.connector
